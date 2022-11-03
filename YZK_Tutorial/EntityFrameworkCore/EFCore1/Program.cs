ad9ae1c3f5be69d842dfb6a54ff33dcd0300a29d
马鑫 <anyangmaxin@163.com>
2022-11-03 20:22:05 +0800

    插入数据

--- YZK_Tutorial/EntityFrameworkCore/EFCore1/Program.cs
+++ YZK_Tutorial/EntityFrameworkCore/EFCore1/Program.cs
@@ -1,2 +1,18 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using EFCore1;
+
 Console.WriteLine("Hello, World!");
+Console.WriteLine("插入数据");
+using (var dbctx = new TestDbContext())
+{
+    Book book = new Book()
+    {
+        Price = 100,
+        PubTime = DateTime.Now.AddMonths(-3),
+        Title = "测试"
+    };
+
+    dbctx.Books.Add(book);
+    //推荐使用异步方法
+    await dbctx.SaveChangesAsync();
+}
