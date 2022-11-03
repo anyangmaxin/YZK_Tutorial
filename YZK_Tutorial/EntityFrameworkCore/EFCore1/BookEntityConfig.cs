c8bf9c9cb8091b9747d8e4d03bbffa1a81cdbfca
马鑫 <anyangmaxin@163.com>
2022-11-03 18:37:36 +0800

    增加对应的实体的数据库配置文件

---
+++ YZK_Tutorial/EntityFrameworkCore/EFCore1/BookEntityConfig.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFCore1
+{
+    public class BookEntityConfig : IEntityTypeConfiguration<Book>
+    {
+        public void Configure(EntityTypeBuilder<Book> builder)
+        {
+            //throw new NotImplementedException();
+            builder.ToTable("T_Books");
+        }
+    }
+}
