70a5ab6b0fe150623cfa778417236f0c6a58c2f0
马鑫 <anyangmaxin@163.com>
2022-11-03 17:10:12 +0800

    增加Book类

---
+++ YZK_Tutorial/EntityFrameworkCore/EFCore1/Book.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFCore1
+{
+    public class Book
+    {
+        /// <summary>
+        /// 主键
+        /// </summary>
+        public long Id { get; set; }
+
+        /// <summary>
+        /// 标题
+        /// </summary>
+        public string Title { get; set; }
+
+        /// <summary>
+        /// 发布日期
+        /// </summary>
+        public DateTime PubTime { get; set; }
+
+        /// <summary>
+        /// 单价
+        /// </summary>
+        public double Price { get; set; }
+    }
+}
