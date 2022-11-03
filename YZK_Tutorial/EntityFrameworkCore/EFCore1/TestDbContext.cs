69f4e1f035242701fbdfcc2e9f3046f5ea84ad37
马鑫 <anyangmaxin@163.com>
2022-11-03 19:33:33 +0800

    Add-Migration  初始化迁移，以及首次生成数据 库

---
+++ YZK_Tutorial/EntityFrameworkCore/EFCore1/TestDbContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFCore1
+{
+    public class TestDbContext : DbContext
+    {
+        public DbSet<Book> Books { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+           
+            string connStr = "Server=.;DataBase=TrainDB;Trusted_Connection=True;MultipleActiveResultSets=True;";
+            optionsBuilder.UseSqlServer(connStr);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            //当前程序集下的配置文件
+            modelBuilder.ApplyConfigurationsFromAssembly(this.GetType().Assembly);
+        }
+
+    }
+}
