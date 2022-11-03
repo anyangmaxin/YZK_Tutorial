1064eaefcd11793923744b23b0d13f9ea6e3d8b4
马鑫 <anyangmaxin@163.com>
2022-11-03 20:31:58 +0800

    删除数据 ，查询数据

--- YZK_Tutorial/EntityFrameworkCore/EFCore1/Program.cs
+++ YZK_Tutorial/EntityFrameworkCore/EFCore1/Program.cs
@@ -21,16 +21,47 @@ Console.WriteLine("Hello, World!");
 //    Console.WriteLine("插入数据***************");
 //}
 
+//{
+//    Console.WriteLine("更新数据");
+//    using (var dbctx = new TestDbContext())
+//    {
+//        var model=dbctx.Books.OrderByDescending(o => o.Id).FirstOrDefault();
+//        model.Price += 20;
+
+//        //推荐使用异步方法
+//        await dbctx.SaveChangesAsync();
+
+//    }
+//    Console.WriteLine("更新数据***************");
+//}
+
+
+
 {
-    Console.WriteLine("更新数据");
+    Console.WriteLine("删除数据");
     using (var dbctx = new TestDbContext())
     {
-        var model=dbctx.Books.OrderByDescending(o => o.Id).FirstOrDefault();
-        model.Price += 20;
-
+        var model = dbctx.Books.OrderByDescending(o => o.Id).FirstOrDefault();
+        // model.Price += 20;
+        dbctx.Books.Remove(model);
         //推荐使用异步方法
         await dbctx.SaveChangesAsync();
 
     }
-    Console.WriteLine("更新数据***************");
+    Console.WriteLine("删除数据***************");
+}
+
+
+{
+    Console.WriteLine("查询数据");
+    using (var dbctx = new TestDbContext())
+    {
+        var models = dbctx.Books.ToList();
+        foreach (var item in models)
+        {
+            Console.WriteLine($"{item.Id}-{item.Title}-{item.Price}");
+        }
+
+    }
+    Console.WriteLine("查询数据***************");
 }
