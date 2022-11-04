f95a9b02f25b409d95e16282ae9e418f045e482a
马鑫 <anyangmaxin@163.com>
2022-11-04 17:35:53 +0800

    增加文章与评论实体

---
+++ YZK_Tutorial/EntityFrameworkCore/EFCoreOneToMany/Article.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFCoreOneToMany
+{
+    public class Article
+    {
+        public long Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Content { get; set; }
+
+        public List<Comment> Comments { get; set; } = new List<Comment>();
+    }
+}
---
+++ YZK_Tutorial/EntityFrameworkCore/EFCoreOneToMany/Comment.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFCoreOneToMany
+{
+    public class Comment
+    {
+        public long Id { get; set; }
+
+        public Article Article { get; set; }
+
+        public string Message { get; set; }
+    }
+}
