395cb747639d873361ea93f9041361596b703acc
马鑫 <anyangmaxin@163.com>
2022-11-04 17:38:08 +0800

    增加文章实体对应的配置类

---
+++ YZK_Tutorial/EntityFrameworkCore/EFCoreOneToMany/ArticleConfig.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFCoreOneToMany
+{
+    public class ArticleConfig : IEntityTypeConfiguration<Article>
+    {
+        public void Configure(EntityTypeBuilder<Article> builder)
+        {
+           //这里可以配置对应表，对应字段 相关约束等信息
+        }
+    }
+}
