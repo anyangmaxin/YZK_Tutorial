f487b1c740edb0fb151ce538a37c3f084f14a8fd
马鑫 <anyangmaxin@163.com>
2022-11-04 17:39:22 +0800

    添加评论实体配置类

---
+++ YZK_Tutorial/EntityFrameworkCore/EFCoreOneToMany/CommentConfig.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFCoreOneToMany
+{
+    public class CommentConfig : IEntityTypeConfiguration<CommentConfig>
+    {
+        public void Configure(EntityTypeBuilder<CommentConfig> builder)
+        {
+            //这里可以配置对应表，对应字段 相关约束等信息
+        }
+    }
+}
