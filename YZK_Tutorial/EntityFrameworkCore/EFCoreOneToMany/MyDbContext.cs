89820f9e2c6874b73c0752369d3eb760b1192f7f
马鑫 <anyangmaxin@163.com>
2022-11-04 17:46:04 +0800

    增加MyDbContext 设置表实体，以及数据库连接

---
+++ YZK_Tutorial/EntityFrameworkCore/EFCoreOneToMany/MyDbContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+
+
+namespace EFCoreOneToMany
+{
+    public class MyDbContext : DbContext
+    {
+
+        public DbSet<Article> Articles { get; set; }
+
+        public DbSet<Comment> Comments { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            //配置数据库连接
+            string connStr = "Server=.;DataBase=TrainDB;Trusted_Connection=True;MultipleActiveResultSets=True;";
+            optionsBuilder.UseSqlServer(connStr);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            //指示配置类在此程序集下
+            modelBuilder.ApplyConfigurationsFromAssembly(this.GetType().Assembly);
+        }
+    }
+}
