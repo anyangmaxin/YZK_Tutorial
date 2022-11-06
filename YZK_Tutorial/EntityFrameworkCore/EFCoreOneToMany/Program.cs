d4cb198a9a68ea3589ac98151554d50da19baade
马鑫 <anyangmaxin@163.com>
2022-11-06 15:15:56 +0800

    输出Article

--- YZK_Tutorial/EntityFrameworkCore/EFCoreOneToMany/Program.cs
+++ YZK_Tutorial/EntityFrameworkCore/EFCoreOneToMany/Program.cs
@@ -1,2 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using EFCoreOneToMany;
+
 Console.WriteLine("Hello, World!");
+
+IEnumerable<Article> articles;
+using (MyDbContext db = new MyDbContext())
+{
+    articles = db.Articles.ToList();
+}
+
+foreach (Article article in articles)
+{
+    Console.WriteLine($"{article.Id}-{article.Title}-{article.Content}");
+}
