290fc3cf36579c016f2b0363a77928284aa743f7
马鑫 <anyangmaxin@163.com>
2022-11-06 15:28:02 +0800

    忽略全局筛选器  IgnoreQueryFilters

--- YZK_Tutorial/EntityFrameworkCore/EFCoreOneToMany/Program.cs
+++ YZK_Tutorial/EntityFrameworkCore/EFCoreOneToMany/Program.cs
@@ -1,12 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using EFCoreOneToMany;
+using Microsoft.EntityFrameworkCore;
 
 Console.WriteLine("Hello, World!");
 
 IEnumerable<Article> articles;
 using (MyDbContext db = new MyDbContext())
 {
-    articles = db.Articles.Take(20).ToList();
+    //articles = db.Articles.Take(20).ToList();
+    //忽略全局筛选器
+    articles = db.Articles.IgnoreQueryFilters().Take(10).ToList();
 }
 
 foreach (Article article in articles)
