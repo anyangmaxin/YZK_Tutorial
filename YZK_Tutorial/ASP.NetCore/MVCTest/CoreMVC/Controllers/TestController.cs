c4dc5e9877d665d55619660b6892e6eb5c20a840
马鑫 <anyangmaxin@163.com>
2022-11-09 20:45:46 +0800

    增加TestController以及Demo1  Action

---
+++ YZK_Tutorial/ASP.NetCore/MVCTest/CoreMVC/Controllers/TestController.cs
@@ -0,0 +1,19 @@
+﻿using CoreMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreMVC.Controllers
+{
+    public class TestController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Demo1()
+        {
+            var model = new Person("Iron Man", true, new DateTime(1999, 9, 9));
+            return View(model);
+        }
+    }
+}
