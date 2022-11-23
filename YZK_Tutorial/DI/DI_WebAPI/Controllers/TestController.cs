97f32d0b032bfb46efbd0e8fd8e936ea8a5e1b0e
马鑫 <anyangmaxin@163.com>
2022-11-23 20:36:46 +0800

    在测试接口中通过构造函数 注入计算类

---
+++ YZK_Tutorial/DI/DI_WebAPI/Controllers/TestController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DI_WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        private readonly Calculator calculator;
+
+        public TestController(Calculator calculator)
+        {
+            this.calculator = calculator;
+        }
+
+        [HttpPost]
+        [Route("Add")]
+        public IActionResult Add(int x, int y)
+        {
+            //return calculator.Add(3, 5);
+            return Ok(this.calculator.Add(x, y));
+        }
+
+        [HttpPost]
+        [Route("Add2")]
+        public IActionResult Add2(int x, int y)
+        {
+            //return calculator.Add(3, 5);
+            return Ok(this.calculator.Add(x, y));
+        }
+    }
+}
