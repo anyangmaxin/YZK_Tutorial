af64a351790e0fe89c9efac43c277831abd78b74
马鑫 <anyangmaxin@163.com>
2022-11-23 16:34:16 +0800

    增加一个计算类

---
+++ YZK_Tutorial/DI/DI_WebAPI/Calculator.cs
@@ -0,0 +1,16 @@
+﻿namespace DI_WebAPI
+{
+    public class Calculator
+    {
+        /// <summary>
+        /// 加法运算
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <returns></returns>
+        public int Add(int x, int y)
+        {
+            return x + y;
+        }
+    }
+}
