267463be5ed00f5d7f1f8143f46d6ce362258c65
马鑫 <anyangmaxin@163.com>
2022-11-24 11:14:33 +0800

    使用FromeServices标记，这样在用到的时才构造服务

--- YZK_Tutorial/DI/DI_WebAPI/Controllers/TestController.cs
+++ YZK_Tutorial/DI/DI_WebAPI/Controllers/TestController.cs
@@ -7,15 +7,22 @@ namespace DI_WebAPI.Controllers
     [ApiController]
     public class TestController : ControllerBase
     {
-        private readonly Calculator calculator;
-        private readonly TestService testService;
+        //private readonly Calculator calculator;
+        //private readonly TestService testService;
 
-        public TestController(Calculator calculator, TestService testService)
+        //public TestController(Calculator calculator, TestService testService)
+        //{
+        //    this.calculator = calculator;
+        //    this.testService = testService;
+        //}
+
+
+        private readonly Calculator calculator;
+
+        public TestController(Calculator calculator)
         {
             this.calculator = calculator;
-            this.testService = testService;
         }
-
         [HttpPost]
         public IActionResult Add(int x, int y)
         {
@@ -30,10 +37,21 @@ namespace DI_WebAPI.Controllers
             return Ok(this.calculator.Add(x, y));
         }
 
+        //[HttpGet]
+        //public IActionResult GetFileCount()
+        //{
+        //    return Ok(this.testService.Count());
+        //}
+
+        /// <summary>
+        /// 需要用到的时候再构造相应服务，不影响全局加载
+        /// </summary>
+        /// <param name="testService"></param>
+        /// <returns></returns>
         [HttpGet]
-        public IActionResult GetFileCount()
+        public IActionResult GetFileCount([FromServices]TestService testService)
         {
-            return Ok(this.testService.Count());
+            return Ok(testService.Count());
         }
     }
 }
