874d7f13e787a0448ff778cc947608fe73cf0179
马鑫 <anyangmaxin@163.com>
2022-11-25 17:26:53 +0800

    测试跳转时区

--- YZK_Tutorial/Cache/Cache_WebAPI1/Controllers/WeatherForecastController.cs
+++ YZK_Tutorial/Cache/Cache_WebAPI1/Controllers/WeatherForecastController.cs
@@ -39,7 +39,51 @@ namespace Cache_WebAPI1.Controllers
         [HttpGet]
         public ActionResult<string> Hello(string name)
         {
-            return $"Hello {name}";
+            return $"Hello {name} {DateTime.UtcNow} {DateTime.UtcNow.ToLocalTime()}";
+        }
+
+        [HttpGet]
+        public ActionResult<string> ConvertUtcByTimeZone(int timeZone)
+        {
+            DateTime timeUtc = DateTime.UtcNow;
+            try
+            {
+
+                return timeUtc.AddHours(timeZone).ToString();
+            }
+            catch (TimeZoneNotFoundException)
+            {
+                Console.WriteLine("The registry does not define the Central Standard Time zone.");
+            }
+
+            return timeUtc.ToString();
+        }
+
+        [HttpGet]
+        public ActionResult<string> ConvertUtc(int timeZone)
+        {
+            DateTime timeUtc = DateTime.UtcNow;
+            try
+            {
+
+                TimeZoneInfo cstZone = TimeZoneInfo.FindSystemTimeZoneById("Central Standard Time");
+
+                //DateTime cstTime = TimeZoneInfo.ConvertTimeFromUtc(timeUtc, cstZone);
+                DateTime cstTime = TimeZoneInfo.ConvertTimeFromUtc(timeUtc, cstZone);
+                Console.WriteLine("The date and time are {0} {1}.",
+                                  cstTime,
+                                  cstZone.IsDaylightSavingTime(cstTime) ?
+                                          cstZone.DaylightName : cstZone.StandardName);
+            }
+            catch (TimeZoneNotFoundException)
+            {
+                Console.WriteLine("The registry does not define the Central Standard Time zone.");
+            }
+            catch (InvalidTimeZoneException)
+            {
+                Console.WriteLine("Registry data on the Central Standard Time zone has been corrupted.");
+            }
+            return timeUtc.ToString();
         }
     }
 }
