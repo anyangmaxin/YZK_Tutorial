31e7d699a32ee11a20a9e60562dce17b657b8c39
马鑫 <anyangmaxin@163.com>
2022-11-29 16:05:53 +0800

    增加测试接口

---
+++ YZK_Tutorial/Cache/Cache_Redis/Controllers/TestController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Cache_Redis.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<string> Index()
+        {
+            return "Redis分布式缓存 ";
+        }
+    }
+}
