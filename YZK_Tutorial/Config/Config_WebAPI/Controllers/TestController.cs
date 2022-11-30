a89f36b9ed2ac031ee7096513c7d000d4690291c
马鑫 <anyangmaxin@163.com>
2022-11-30 20:50:23 +0800

    通过注入的IWebHostEnvironment内置属性来获取配置参数,就是app.Environment

--- YZK_Tutorial/Config/Config_WebAPI/Controllers/TestController.cs
+++ YZK_Tutorial/Config/Config_WebAPI/Controllers/TestController.cs
@@ -7,6 +7,13 @@ namespace Config_WebAPI.Controllers
     [ApiController]
     public class TestController : ControllerBase
     {
+        private readonly IWebHostEnvironment _environment;
+
+        public TestController(IWebHostEnvironment environment)
+        {
+            _environment = environment;
+        }
+
         /// <summary>
         /// 从系统获取用户环境变量值 
         /// </summary>
@@ -15,6 +22,17 @@ namespace Config_WebAPI.Controllers
         public ActionResult<string> GetSysPath()
         {
             return Environment.GetEnvironmentVariable("Path");
+        }
+
+        /// <summary>
+        /// 通过注入的IWebHostEnvironment内置属性来获取配置参数,就是app.Environment
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+
+        public ActionResult<string> GetEnvValue()
+        {
+            return _environment.EnvironmentName;
         }
     }
 }
