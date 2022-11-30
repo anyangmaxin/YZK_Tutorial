fdb3662c39d4487a87265cb45bac13b031b827ed
马鑫 <anyangmaxin@163.com>
2022-11-30 20:35:19 +0800

    从系统环境变量获取值

---
+++ YZK_Tutorial/Config/Config_WebAPI/Controllers/TestController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Config_WebAPI.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        /// <summary>
+        /// 从系统环境变量获取值 
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public ActionResult<string> GetSysPath()
+        {
+            return Environment.GetEnvironmentVariable("Path");
+        }
+    }
+}
