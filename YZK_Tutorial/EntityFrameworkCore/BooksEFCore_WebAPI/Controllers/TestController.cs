021eb268bec45580273127f0c4b5e29bb16dff51
马鑫 <anyangmaxin@163.com>
2022-12-02 17:06:41 +0800

    配置及测试数据库连接成功

---
+++ YZK_Tutorial/EntityFrameworkCore/BooksEFCore_WebAPI/Controllers/TestController.cs
@@ -0,0 +1,24 @@
+﻿using BooksEFCore;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BooksEFCore_WebAPI.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+
+        private readonly MyDbContext myDbContext;
+        public TestController(MyDbContext myDbContext)
+        {
+            this.myDbContext = myDbContext;
+        }
+
+        [HttpGet]
+        public ActionResult<int> GetCount()
+        {
+            return myDbContext.Books.Count();
+        }
+    }
+}
