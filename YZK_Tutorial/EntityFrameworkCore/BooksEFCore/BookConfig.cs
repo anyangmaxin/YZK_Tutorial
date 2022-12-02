37050200b4931dcec9f7025ca8a12a8108a9366b
马鑫 <anyangmaxin@163.com>
2022-12-02 12:11:19 +0800

    增加BookConfig配置类，并指定表名

---
+++ YZK_Tutorial/EntityFrameworkCore/BooksEFCore/BookConfig.cs
@@ -0,0 +1,22 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BooksEFCore
+{
+    internal class BookConfig : IEntityTypeConfiguration<Book>
+    {
+
+        public void Configure(EntityTypeBuilder<Book> builder)
+        {
+            //throw new NotImplementedException();
+            //指定表名
+            builder.ToTable("T_Books");
+        }
+    }
+}
