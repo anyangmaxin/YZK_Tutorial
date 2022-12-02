055686c2da996a23bc497e7e11292d418672c151
马鑫 <anyangmaxin@163.com>
2022-12-02 14:26:12 +0800

    从运行的当前类中获取配置，但是数据库连接字符串不固定写在类库中。

--- YZK_Tutorial/EntityFrameworkCore/BooksEFCore/MyDbContext.cs
+++ YZK_Tutorial/EntityFrameworkCore/BooksEFCore/MyDbContext.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,5 +11,23 @@ namespace BooksEFCore
 {
     internal class MyDbContext : DbContext
     {
+        public DbSet<Book> Books { get; set; }
+
+        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        //{
+        //    base.OnConfiguring(optionsBuilder);
+        //    optionsBuilder.UseSqlserver("");
+        //}
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            //指定从哪个程序集来加载配置
+            //控制台程序 可以从入口程序 获取 
+            //modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetEntryAssembly());
+            //从运行的当前类中获取 配置
+            modelBuilder.ApplyConfigurationsFromAssembly(this.GetType().Assembly);
+        }
     }
 }
