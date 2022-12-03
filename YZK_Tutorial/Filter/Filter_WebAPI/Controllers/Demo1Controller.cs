c09771bae187b8f46dfb077f7c8dfdc93cf58a84
马鑫 <anyangmaxin@163.com>
2022-12-03 21:07:42 +0800

    测试直接抛出异常

---
+++ YZK_Tutorial/Filter/Filter_WebAPI/Controllers/Demo1Controller.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Filter_WebAPI.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class Demo1Controller : ControllerBase
+    {
+        /// <summary>
+        /// 用于测试返回异常信息
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public string GetFile()
+        {
+            string s = System.IO.File.ReadAllText("f:1.txt");
+            return s;
+        }
+    }
+}
