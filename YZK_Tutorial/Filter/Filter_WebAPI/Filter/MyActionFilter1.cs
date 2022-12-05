c3143b19b9ba5b81c368c477778b9959ae51c1b7
马鑫 <anyangmaxin@163.com>
2022-12-05 12:22:21 +0800

    增加自定义ActionFilter

---
+++ YZK_Tutorial/Filter/Filter_WebAPI/Filter/MyActionFilter1.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace Filter_WebAPI.Filter
+{
+    public class MyActionFilter1 : IAsyncActionFilter
+    {
+        public Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
