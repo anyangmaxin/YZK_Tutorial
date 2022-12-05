bad259b111a7395a2b12a0334a6dd73a296d58a8
马鑫 <anyangmaxin@163.com>
2022-12-05 18:22:25 +0800

    填充MyActionFilter1逻辑

--- YZK_Tutorial/Filter/Filter_WebAPI/Filter/MyActionFilter1.cs
+++ YZK_Tutorial/Filter/Filter_WebAPI/Filter/MyActionFilter1.cs
@@ -4,9 +4,19 @@ namespace Filter_WebAPI.Filter
 {
     public class MyActionFilter1 : IAsyncActionFilter
     {
-        public Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+        public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
         {
-            throw new NotImplementedException();
+            //throw new NotImplementedException();
+            Console.WriteLine("MyActionFilter1开始执行：");
+            ActionExecutedContext r = await next();
+            if (r.Exception != null)
+            {
+                Console.WriteLine("MyActionFilter1执行失败：");
+            }
+            else
+            {
+                Console.WriteLine("MyActionFilter1执行成功：");
+            }
         }
     }
 }
