6f706c411cfc59642eb8d57895a5b9ffb9635728
马鑫 <anyangmaxin@163.com>
2022-12-10 11:51:05 +0800

    修改连接串配置，进行数据库迁移

---
+++ YZK_Tutorial/Filter/Filter_WebAPI/Controllers/Demo2Controller.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Filter_WebAPI.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class Demo2Controller : ControllerBase
+    {
+        private readonly MyDbContext myDbContext;
+
+        public Demo2Controller(MyDbContext myDbContext)
+        {
+            this.myDbContext = myDbContext;
+        }
+
+        [HttpPost]
+        public String Test2()
+        {
+            myDbContext.Books.Add(new Model.Book { Id = 1, Title = "我最棒" });
+            myDbContext.SaveChangesAsync();
+            return "";
+        }
+    }
+}
