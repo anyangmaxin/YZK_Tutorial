ed7496c7997f7b9e95d0fb00f65f07f1a184daa8
马鑫 <anyangmaxin@163.com>
2022-12-10 22:27:11 +0800

    增加TestMiddleware类用于测试

---
+++ YZK_Tutorial/Middleware/Middleware1/TestMiddleware.cs
@@ -0,0 +1,30 @@
+﻿namespace Middleware1
+{
+    public class TestMiddleware
+    {
+        private readonly RequestDelegate next;
+
+        /// <summary>
+        /// 约定需要一个RequestDelegate的参数
+        /// </summary>
+        /// <param name="next"></param>
+        public TestMiddleware(RequestDelegate next)
+        {
+            this.next = next;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="context"></param>
+        /// <returns></returns>
+        public async Task InvokeAsync(HttpContext context)
+        {
+            context.Response.WriteAsync("TestMiddleware Start <br/>");
+            //执行下一个中间件
+            await this.next(context);
+            context.Response.WriteAsync("TestMiddleware End <br/>");
+
+        }
+    }
+}
