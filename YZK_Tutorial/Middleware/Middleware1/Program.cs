38a4118d2e829f3f0a64105bca8bea4f6475fdff
马鑫 <anyangmaxin@163.com>
2022-12-10 18:29:29 +0800

    第一个使用Map  Use  Run的测试输出 文本的中间件

--- YZK_Tutorial/Middleware/Middleware1/Program.cs
+++ YZK_Tutorial/Middleware/Middleware1/Program.cs
@@ -1,7 +1,25 @@
+using Microsoft.AspNetCore.Builder;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
-app.MapGet("/test", () => "Hello Test!");
+//app.MapGet("/test", () => "Hello Test!");
+
+app.Map("/test", async (pipeBuilder) =>
+{
+    pipeBuilder.Use(async (context, next) =>
+    {
+        context.Response.ContentType = "text/html";
+        //前逻辑  输出 内容
+        await context.Response.WriteAsync("1 Start <br/>");
+        //请求下一个中间件
+        await next();
+
+        //后逻辑  输出 内容
+        await context.Response.WriteAsync("1 End <br/>");
+
+    });
+});
 
 app.Run();
