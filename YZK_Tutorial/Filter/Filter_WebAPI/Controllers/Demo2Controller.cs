79018828c84c9c415e7898a44cfa58c694d43c3c
马鑫 <anyangmaxin@163.com>
2022-12-10 12:17:13 +0800

    TransactionScope   同一事务实现要么成功，要么失败

--- YZK_Tutorial/Filter/Filter_WebAPI/Controllers/Demo2Controller.cs
+++ YZK_Tutorial/Filter/Filter_WebAPI/Controllers/Demo2Controller.cs
@@ -49,7 +49,22 @@ namespace Filter_WebAPI.Controllers
         }
 
 
-  
+        /// <summary>
+        /// 同一事务实现要么成功，要么失败
+        /// </summary>
+        /// <returns></returns>
+        [HttpPost]
+        public async Task<string> AddBookPerson2()
+        {
+            using (TransactionScope scope = new TransactionScope())
+            {
+                myDbContext.Books.Add(new Model.Book { Title = "我最棒" + DateTime.Now });
+                await myDbContext.SaveChangesAsync();
+                myDbContext.Persons.Add(new Model.Person { Id = 1, Name = "姓名" + DateTime.Now, Age = 18 + Random.Shared.Next(1, 10) });
+                await myDbContext.SaveChangesAsync();
+                return "OK";
+            }
+        }
 
     }
 }
