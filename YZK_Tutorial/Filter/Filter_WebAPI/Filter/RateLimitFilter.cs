6cdcf62aaf1e34fc84501495a17a11e0e21cb97d
马鑫 <anyangmaxin@163.com>
2022-12-10 17:33:32 +0800

    增加自定义限流Filter

---
+++ YZK_Tutorial/Filter/Filter_WebAPI/Filter/RateLimitFilter.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace Filter_WebAPI.Filter
+{
+    /// <summary>
+    /// 自定义限流Filter
+    /// </summary>
+    public class RateLimitFilter : IAsyncActionFilter
+    {
+        private readonly IMemoryCache memoryCache;
+
+        public RateLimitFilter(IMemoryCache memoryCache)
+        {
+            this.memoryCache = memoryCache;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="context"></param>
+        /// <param name="next"></param>
+        /// <returns></returns>
+        public Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+        {
+            //throw new NotImplementedException();
+            string removeIP = context.HttpContext.Connection.RemoteIpAddress.ToString();
+            string cacheKey = $"LastVisitTick_{removeIP}";
+            long? lastTick = this.memoryCache.Get<long?>(cacheKey);
+            if (lastTick == null || Environment.TickCount64 - lastTick > 1000) //未找到存储的值 或者是上次请求到现在已经超过1秒
+            {
+                //设置本次请求的时间
+                this.memoryCache.Set(cacheKey, Environment.TickCount64, TimeSpan.FromSeconds(10));
+                //继续执行
+                return next();
+            }
+            else
+            {
+                //返回状态码，不继续执行
+                context.Result = new ContentResult() { StatusCode = 429 };
+                return Task.CompletedTask;
+            }
+        }
+    }
+}
