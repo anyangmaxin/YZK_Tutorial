00f0c11576cba9211c2763c2b2c8dbe6e6a982b7
马鑫 <anyangmaxin@163.com>
2022-12-10 17:44:20 +0800

    增加测试接口

---
+++ YZK_Tutorial/Filter/Filter_WebAPI/Controllers/Demo3Controller.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Filter_WebAPI.Controllers
+{
+    /// <summary>
+    /// 用于测试限流
+    /// </summary>
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    [ApiExplorerSettings(GroupName = "v2")]
+    public class Demo3Controller : ControllerBase
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public string Index()
+        {
+            return "OK";
+        }
+    }
+}
