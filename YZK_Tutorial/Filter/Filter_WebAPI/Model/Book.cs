7ab5a2691d90f8f56bfa143e000744a8478ffe06
马鑫 <anyangmaxin@163.com>
2022-12-10 11:20:05 +0800

    增加实体book与Person，增加数据库上下文类，

---
+++ YZK_Tutorial/Filter/Filter_WebAPI/Model/Book.cs
@@ -0,0 +1,9 @@
+﻿namespace Filter_WebAPI.Model
+{
+    public class Book
+    {
+        public long Id { get; set; }
+
+        public string Title { get; set; }   
+    }
+}
---
+++ YZK_Tutorial/Filter/Filter_WebAPI/MyDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Filter_WebAPI.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace Filter_WebAPI
+{
+    public class MyDbContext : DbContext
+    {
+        public MyDbContext(DbContextOptions<MyDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Person> Persons { get; set; }
+
+
+    }
+}
