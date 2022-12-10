5f6102e41dcbdc846d7a3529688debed995f3cdd
马鑫 <anyangmaxin@163.com>
2022-12-10 17:48:59 +0800

    优化限流Filter

--- YZK_Tutorial/Filter/Filter_WebAPI/Filter/RateLimitFilter.cs
+++ YZK_Tutorial/Filter/Filter_WebAPI/Filter/RateLimitFilter.cs
@@ -30,7 +30,7 @@ namespace Filter_WebAPI.Filter
             long? lastTick = this.memoryCache.Get<long?>(cacheKey);
             if (lastTick == null || Environment.TickCount64 - lastTick > 1000) //未找到存储的值 或者是上次请求到现在已经超过1秒
             {
-                //设置本次请求的时间
+                //设置本次请求的时间，以及10秒后删除多余的缓存 数据
                 this.memoryCache.Set(cacheKey, Environment.TickCount64, TimeSpan.FromSeconds(10));
                 //继续执行
                 return next();
@@ -38,7 +38,7 @@ namespace Filter_WebAPI.Filter
             else
             {
                 //返回状态码，不继续执行
-                context.Result = new ContentResult() { StatusCode = 429 };
+                context.Result = new ContentResult() { StatusCode = 429, Content = "访问过于频繁" };
                 return Task.CompletedTask;
             }
         }
